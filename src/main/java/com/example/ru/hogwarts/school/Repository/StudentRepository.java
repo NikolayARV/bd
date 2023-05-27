d302c5925ead76ad8e8f97ccd12d4485bb6b3d8a
Nikolay <razenkov.rna@mail.ru>
2023-05-27 06:19:18 -0700

    Пагинация

--- src/main/java/com/example/ru/hogwarts/school/Repository/StudentRepository.java
+++ src/main/java/com/example/ru/hogwarts/school/Repository/StudentRepository.java
@@ -3,12 +3,23 @@ package com.example.ru.hogwarts.school.Repository;
 import com.example.ru.hogwarts.school.model.Student;
 import org.aspectj.weaver.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Set;
 
-public interface StudentRepository extends JpaRepository<Student,Long> {
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
     Set<Student> findByAge(Integer age);
+
     Set<Student> findByAgeBetween(int age, int age2);
 
+    @Query(value = "SELECT COUNT (*) FROM STUDENT", nativeQuery = true)
+    Integer getStudentsCount();
+
+    @Query(value = "SELECT AVG(age) FROM STUDENT", nativeQuery = true)
+    Integer getAvgAge();
+    @Query(value = "SELECT * FROM STUDENT ORDER BY age LIMIT 5", nativeQuery = true)
+    Set<Student> findFiveYoungestStudents();
 }
